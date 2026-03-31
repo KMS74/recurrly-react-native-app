66efa9c23b8f0c638910a38e4b5c372bc01cc1ee
Karim Shabana <karim.shabana.dev@gmail.com>
2026-03-31 16:06:48 +0200

    feat: add global styles and configure NativeWind for styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import "@/global.css";
 import { Text, View } from "react-native";
 
-export default function Index() {
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-2xl font-bold text-primary">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
