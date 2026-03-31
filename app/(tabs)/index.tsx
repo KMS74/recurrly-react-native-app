66a7cf2db5a1d411603990de98bf30f0e0535cb0
Karim Shabana <karim.shabana.dev@gmail.com>
2026-04-01 00:53:28 +0200

    feat: add custom fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,36 +9,27 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function Home() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-7xl font-bold font-sans-extrabold">Home</Text>
 
       <Link
         href="/onboarding"
-        className="text-xl font-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
+        className="text-xl font-bold font-sans-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
       >
         Go to Onboarding
       </Link>
 
       <Link
         href="/(auth)/sign-up"
-        className="text-xl font-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
+        className="text-xl font-bold font-sans-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
       >
         Go to Sign Up
       </Link>
 
       <Link
         href="/(tabs)/subscriptions"
-        className="text-xl font-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
+        className="text-xl font-bold font-sans-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
       >
         Go to Subscriptions
-      </Link>
-
-      <Link
-        href="/subscriptions/claude"
-        className="text-xl font-bold text-white mt-4 rounded-lg bg-primary px-4 py-2"
-      >
-        Claude Subscription Details
       </Link>
     </SafeAreaView>
   );
